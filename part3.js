fc58049885c764a1ebd6dae0ba4e800bc9850180
HerbertJames831 <HerbertJames831@gmail.com>
2022-02-07 22:22:31 +0000

    Upload Lab 3 Q3

---
+++ part3.js
@@ -0,0 +1,14 @@
+//In this exercise, the aim is to implement the same functionality as exercise 2 using an interface instead.
+//A class will be required to implement the interface. To complete this exercise you will need to do the following.
+//Create an interface and add the following to it:
+//(a)  An array of strings
+//(b)  An addTask function which:
+// (i) Receives a string as a parameter
+//(ii) Prints a message in the console indicating the insertion
+// (iii) Returns the number of elements in the array after the insertion
+//(c) Create a listallTasks function:
+//(i) Prints in the console all the tasks of the list
+//(d) Create a deleteTask function:
+//(i) Receives a string as a parameter
+//(ii) Prints a message in the console indicating the deletion
+//(iii) Returns the number of elements in the array after the deletion
---
+++ todointerface.ts
@@ -0,0 +1,12 @@
+export interface todoInterface{
+
+    tasks: Array <string>;
+
+    addTask(task:string):number;
+    listAllItems(): void;
+    deleteTask(task:string):number;
+
+
+
+
+}
